ad4a882a2ac2c379ad87f96ebbcfb9460b5e8780
wendykarume <wendyjosephine.karume@strathmore.edu>
2018-01-23 11:53:55 +0300

    [Update]-work

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package calculator;
-
-/**
- *
- * @author karume wendy
- */
-public class Calculator {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+import javax.swing.*;
+public class Calculator{
+    public static void main(String[]args){
+        Addition g = new Addition();
+        g.setSize(250, 400);
+        g.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        g.setVisible(true);
+        g.setResizable(false);
     }
-    
 }
